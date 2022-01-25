67143af9e985ff27dcfdb862333d161e007f2fb4
Ibrahim Mohamed <ia.mohamed02@gmail.com>
2022-01-25 02:09:47 +0000

    update express server

--- express_server.js
+++ express_server.js
@@ -4,9 +4,9 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
-function generateRandomString() {
-
-}
+const generateRandomString = function() {
+  return Math.random().toString(36).substr(2, 8);
+  };
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -31,6 +31,17 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`urls/${shortURL}`);
 });
 
 app.get("/urls/new", (req, res) => {
